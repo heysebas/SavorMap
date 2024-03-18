db4feb73c832a7c370a518aa22b1e562f8dd11d3
sebaz grisales <sebazgrisales@gmail.com>
2024-03-18 08:50:45 -0500

    Creaciones De Botones

--- app/src/main/java/com/hey/savormap/MainActivity.kt
+++ app/src/main/java/com/hey/savormap/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.hey.savormap
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,21 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    }
+
+    fun goRegister(v:View) {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivities(arrayOf(intent))
+    }
+
+    fun goLogin(v:View) {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivities(arrayOf(intent))
+    }
+
+    fun goCreateSite(v:View) {
+        val intent = Intent(this, CreateSiteActivity::class.java)
+        startActivities(arrayOf(intent))
     }
 }
